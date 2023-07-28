fde094a03a70b4644b04338e0a7e4490e6eab060
Manikanta7111 <manikanta.sontanam35@gmail.com>
2023-07-28 14:15:08 +0530

    L

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 // ignore_for_file: avoid_print, no_leading_underscores_for_local_identifiers
 import 'package:flutter/material.dart';
-//import 'package:dio/dio.dart';
+//import 'package:dio/dio.dart'; 
 import 'package:my_app/login.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatefulWidget { 
   const MyApp({super.key});
 
   @override
